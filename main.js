737ae23fd70bbc5ac0c72b94df1dfa92de2b7b0e
Oleh <tsuria.dev@gmail.com>
2023-01-30 19:17:03 +0100

    First task

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 import {testForTask1} from "./testForTask1.js";
-{
-testForTask1({"a": 32});
-testForTask1(123);
-testForTask1([43,5,'dar',{"a":21}]);
-}
+
+testForTask1();
--- testForTask1.js
+++ testForTask1.js
@@ -1,4 +1,3 @@
-
 function assert(object){
     if(typeof object === "object"){
         let oldLength = Object.keys(object).length;
@@ -7,15 +6,19 @@ function assert(object){
         newObject = Object.assign(newObject,object);
         let newLength = Object.keys(newObject).length;
         if(oldLength !== newLength) throw new SyntaxError("Як так то блять?");
-    }
-    else throw new SyntaxError("Variable isn't an object");
+    } else throw new SyntaxError("Variable isn't an object");
 }
-export function testForTask1(object){
-    try{
+function catched(object){
+    try {
         assert(object);
         console.log("Test passed successfully");
     }
     catch (Err){
-        console.log("Test Failed.",Err.message);
+        console.log("Test Failed.", Err.message);
     }
+}
+export function testForTask1(){
+    catched({"a": 32});
+    catched(123);
+    catched([43,5,'dar',{"a":21}]);
 }
