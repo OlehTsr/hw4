c830657ff242feb598c320b9d609fab4826e752a
Oleh <tsuria.dev@gmail.com>
2023-02-25 21:04:50 +0100

    fixed task3

--- task3.js
+++ task3.js
@@ -2,71 +2,56 @@ export function getIteratorOverEverySecondLetter(object) {
   if (typeof object === "object") {
     if ("text" in object && typeof object.text === "string") {
       object[Symbol.iterator] = function () {
-        let symbol = -1;
+        let symbol = 1;
+        let returnedSymbol;
         let text = this["text"];
-        let arrayOfSkippedSymbols = [" ", ".", ",", "!", "?", "'", "`","-",";",":"];
+        let arrayOfSkippedSymbols = [" ", ".", ",", "!", "?", "'", "`", "-", ";", ":"];
         return {
           next() {
-            symbol += 2;
-            if (symbol < text.length) {
-              while (arrayOfSkippedSymbols.includes(text[symbol])) {
-                symbol++;
-
-                if (symbol >= text.length) {
-                  return {done: true};
-                }
-              }
-
-              return {done: false, value: text[symbol]};
-            } else return {done: true};
+            while (symbol < text.length) {
+              if (!arrayOfSkippedSymbols.includes(text[symbol]) && typeof text[symbol] !== "undefined") {
+                returnedSymbol = text[symbol]
+                symbol += 2
+                return {done: false, value: returnedSymbol};
+              } else symbol++;
+            }
+            return {done: true};
           }
         }
       }
       return object;
-    } else return 0;
-
-  } else return 0;
-
+    }
+  }
 }
-
+let a = getIteratorBySentences({"text": "   fad sfg. fdsaf"})
+for (let x of a) {
+  console.log(x)
+}
 export function getIteratorByWords(object) {
   if (typeof object === "object") {
     if ("text" in object && typeof object.text === "string") {
       object[Symbol.iterator] = function () {
         let symbol = 0;
         let text = this["text"];
-        let arrayOfSkippedSymbols = [".", ",", "!", "?", "—", ";", ":"];
+        let arrayOfSkippedSymbols = [" ", ".", ",", "!", "?", "—", ";", ":"];
         return {
           next() {
-
             let word = '';
-            while (symbol < text.length) {
-              while (arrayOfSkippedSymbols.includes(text[symbol])) {
-                symbol++;
-
-                if (symbol >= text.length) {
-                  if (word) return {done: false, value: word};
-                  else return {done: true};
-                }
+            while (symbol < text.length){
+              if(arrayOfSkippedSymbols.includes(text[symbol])){
+                if(word) return {done: false, value: word};
+                else symbol++;
               }
-              if (text[symbol] === " ") {
-                if (word) {
-                  symbol++;
-                  return {done: false, value: word};
-                } else symbol++;
-              } else {
-
-                word += text[symbol++];
-              }
+              else word += text[symbol++];
             }
-            if (word) return {done: false, value: word};
-            else return {done: true};
+            if(word) return {done: false, value: word};
+            return {done: true};
           }
         }
       }
       return object;
-    } else return 0;
-  } else return 0
+    }
+  }
 }
 
 export function getIteratorBySentences(object) {
@@ -78,19 +63,13 @@ export function getIteratorBySentences(object) {
         let arrayOfSkippedSymbols = [".", "!", "?"];
         return {
           next() {
-
             let sentence = '';
             while (symbol < text.length) {
-              if (arrayOfSkippedSymbols.includes(text[symbol])) {
-                if (sentence) {
-                  symbol++;
-                  return {done: false, value: sentence};
-                } else symbol++;
-              } else {
-                if (!sentence && text[symbol] === " ") {
-                  symbol++;
-                } else sentence += text[symbol++];
+              if(arrayOfSkippedSymbols.includes(text[symbol])){
+                if(sentence) return {done: false, value: sentence};
+                else symbol++;
               }
+              else (!sentence && text[symbol] === " ") ? symbol++ : sentence += text[symbol++];
             }
             if (sentence) return {done: false, value: sentence};
             else return {done: true};
